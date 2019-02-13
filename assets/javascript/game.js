b133de5230b68c016945f6bc8a394b8be8e1845c
Ben Shurts <bshurts93@gmail.com>
2019-02-12 18:38:13 -0600

    Added keypress functionality to remove guessing of multiple letters or guessing a correct letter twice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,14 @@
 //-----------------// VARIABLES //-----------------//
 var words = ["volkswagen", "bmw", "ford", "chevrolet", "bugatti", "ferrari", "lamborghini", "subaru", "hyundai", "honda"];
+var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
 var wordBox = document.getElementById("word-box");
 var guessedLettersBox = document.getElementById("guessed-letters");
+var messageBox = document.getElementById("message");
 
 var correctGuesses = [];
 var incorrectGuesses = [];
 
-var guessCount = 13;
+var guessCount = 10;
 var currentWord;
 
 //-----------------// FUNCTIONS //-----------------//
@@ -15,12 +17,14 @@ var getRandomWord = function () {
     return words[randNum];
 }
 
-var makeGuess = function(l) {
-    // Decrease guess count and display 
-    guessCount--;
-    document.getElementById("guesses").textContent = guessCount;
-
-    if (currentWord.indexOf(l) === -1) { 
+var makeGuess = function (l) {
+    if (incorrectGuesses.includes(l)) {
+        // If the user has already guessed the selected letter, do nothing
+        console.log("doing nothing!");
+    } else if (currentWord.indexOf(l) === -1) {
+        // Decrease guess count and display 
+        guessCount--;
+        document.getElementById("guesses").textContent = guessCount;
         // Letter is not in the current word, push it to wrong guesses array
         incorrectGuesses.push(l);
         guessedLettersBox.innerHTML = incorrectGuesses;
@@ -29,17 +33,25 @@ var makeGuess = function(l) {
         for (var i = 0; i < currentWord.length; i++) {
             if (currentWord[i] === l) {
                 // Assign the correct letter to the corresponding spot in the array
-                correctGuesses[i] = l;
+                correctGuesses[i] = l.toUpperCase();;
             }
         }
         // Update to DOM
-        wordBox.innerHTML = correctGuesses;
+        wordBox.innerHTML = correctGuesses.join(" ");
+    }
+
+    // If user runs out of guesses, display game over
+    if (guessCount === 0) {
+        messageBox.innerHTML = "Loser!";
     }
 }
 
 
 //-----------------// PSUDO CODE //-----------------//
 
+// SCORING //
+// Get guess count from the declared variable and display to DOM
+document.getElementById("guesses").innerHTML = guessCount;
 // Get word from array
 currentWord = getRandomWord(words);
 
@@ -51,6 +63,12 @@ for (var i = 0; i < currentWord.length; i++) {
 wordBox.innerHTML = correctGuesses.join(" ");
 
 // Set up onkeypress event listener
+document.onkeydown = function (e) {
+    var keypress = e.key;
 
-// On keypress, fire makeGuess function by passing the keypress as the argument
+    // On keypress, fire makeGuess function by passing the keypress as the argument
+    if (alphabet.includes(keypress)) {
+        makeGuess(keypress);
+    }
+}
 
